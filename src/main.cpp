fca67110f0299c6f340b0faf1b0985c307337459
Albert Zeyer <albzey@gmail.com>
2019-01-07 16:43:03 +0100

    initial

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ParseOggVorbis
+//
+//  Created by Albert Zeyer on 07.01.19.
+//  Copyright © 2019 Albert Zeyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
