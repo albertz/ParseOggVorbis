312e9197f2291cab75f4ba75c81c7ae407866c7b
Albert Zeyer <albzey@gmail.com>
2019-01-08 20:19:33 +0100

    more

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@
 
 #include <iostream>
 #include <string>
+#include <map>
 #include <vector>
+#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
 #ifdef __APPLE__
@@ -28,8 +30,8 @@ struct OkOrError {
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
-#define CHECK(v) { if(!(v)) return OkOrError(__FILE__ ":" TOSTRING(__LINE__) ": " #v); }
-#define CHECK_ERR(v) { OkOrError res = (v); if(res.is_error_) return res; }
+#define CHECK(v) do { if(!(v)) return OkOrError(__FILE__ ":" TOSTRING(__LINE__) ": " #v); } while(0)
+#define CHECK_ERR(v) do { OkOrError res = (v); if(res.is_error_) return res; } while(0)
 
 
 inline void endian_swap(uint16_t& x) {
@@ -88,6 +90,15 @@ static uint32_t update_crc(uint32_t crc, uint8_t* buffer, int size){
 	return crc;
 }
 
+// Page + page header is described here:
+// https://xiph.org/vorbis/doc/framing.html
+
+enum {
+	HeaderFlag_Continued = 0x1,
+	HeaderFlag_First = 0x2,
+	HeaderFlag_Last = 0x4
+};
+
 struct __attribute__((packed)) PageHeader {
 	char capture_pattern[4]; // should be "OggS"
 	uint8_t stream_structure_version; // should be 0
@@ -105,8 +116,15 @@ struct Page {
 	uint32_t data_len;
 	uint8_t data[256 * 256];
 	
+	enum ReadHeaderResult { Ok, Eof, Error };
+	ReadHeaderResult read_header(FILE* fp) {
+		size_t c = fread(&header, sizeof(PageHeader), 1, fp);
+		if(c == 1) return ReadHeaderResult::Ok;
+		if(feof(fp)) return ReadHeaderResult::Eof;
+		return ReadHeaderResult::Error;
+	}
+	
 	OkOrError read(FILE* fp) {
-		CHECK(fread(&header, sizeof(PageHeader), 1, fp) == 1);
 		CHECK(memcmp(header.capture_pattern, "OggS", 4) == 0);
 		CHECK(header.stream_structure_version == 0);
 		endian_swap_to_little_endian(header.absolute_granule_pos);
@@ -118,6 +136,8 @@ struct Page {
 		data_len = 0;
 		for(uint8_t i = 0; i < header.page_segments_num; ++i)
 			data_len += segment_table[i];
+		if(header.page_segments_num > 0)
+			CHECK(segment_table[header.page_segments_num - 1] != 255); // packets spanning pages not supported currently...
 		CHECK(fread(data, data_len, 1, fp) == 1);
 		
 		uint32_t expected_crc = header.page_crc_checksum;
@@ -131,14 +151,104 @@ struct Page {
 	}
 };
 
+struct __attribute__((packed)) IdHeader {
+	uint32_t vorbis_version;
+	uint8_t audio_channels;
+	uint32_t audio_sample_rate;
+	uint32_t bitrate_maximum;
+	uint32_t bitrate_nominal;
+	uint32_t bitrate_minimum;
+	uint8_t blocksize;
+	uint8_t framing_flag;
+};
+
+static int ilog(unsigned int v) {
+	int ret = 0;
+	while(v) {
+		++ret;
+		v >>= 1;
+	}
+	return ret;
+};
+
+struct Packet {
+	uint8_t* data;
+	uint32_t data_len; // never more than 256*256
+	
+	OkOrError parse_id() {
+		CHECK(data_len >= 16);
+		uint8_t type = data[0];
+		CHECK(type == 1);
+		CHECK(memcmp(&data[1], "vorbis", 6) == 0);
+		CHECK(data_len - 7 == sizeof(IdHeader));
+		IdHeader header;
+		memcpy(&header, &data[7], sizeof(IdHeader));
+		std::cout << "vorbis version: " << header.vorbis_version
+		<< ", channels: " << (int) header.audio_channels
+		<< ", sample rate: " << header.audio_sample_rate << std::endl;
+		CHECK(header.framing_flag & 1);
+		return OkOrError();
+	}
+	
+	OkOrError parse_comment() {
+		// https://xiph.org/vorbis/doc/v-comment.html
+		CHECK(data_len >= 16);
+		uint8_t type = data[0];
+		CHECK(type == 3);
+		CHECK(memcmp(&data[1], "vorbis", 6) == 0);
+		// ignore for now...
+		return OkOrError();
+	}
+
+	OkOrError parse_setup() {
+		CHECK(data_len >= 16);
+		uint8_t type = data[0];
+		CHECK(type == 5);
+		CHECK(memcmp(&data[1], "vorbis", 6) == 0);
+		// TODO: get the important things...
+		// https://xiph.org/vorbis/doc/Vorbis_I_spec.html
+		return OkOrError();
+	}
+	
+	OkOrError parse_audio() {
+		// TODO ...
+		// https://xiph.org/vorbis/doc/Vorbis_I_spec.html
+		return OkOrError();
+	}
+};
+
+struct StreamInfo {
+	uint32_t packet_counts_;
+	// getCodec(page)
+	// packet buffer...
+	
+	StreamInfo() : packet_counts_(0) {}
+};
+
 struct Reader {
 	char buffer_[255];
 	Page buffer_page_;
+	std::map<uint32_t, StreamInfo> streams_;
+	size_t packet_counts_;
 	FILE* fp_;
 
+	Reader() : packet_counts_(0) {}
+	
 	OkOrError full_read(const char* filename) {
 		CHECK_ERR(_open_file(filename));
-		CHECK_ERR(_read_page());
+		size_t page_count = 0;
+		while(true) {
+			Page::ReadHeaderResult res = buffer_page_.read_header(fp_);
+			if(res == Page::ReadHeaderResult::Ok)
+				CHECK_ERR(_read_page());
+			else if(res == Page::ReadHeaderResult::Eof)
+				break;
+			else
+				return OkOrError("read error");
+			++page_count;
+		}
+		std::cout << "page count: " << page_count << std::endl;
+		std::cout << "packets count: " << packet_counts_ << std::endl;
 		return OkOrError();
 	}
 	
@@ -149,7 +259,46 @@ struct Reader {
 	}
 	
 	OkOrError _read_page() {
-		return buffer_page_.read(fp_);
+		CHECK_ERR(buffer_page_.read(fp_));
+		if(buffer_page_.header.header_type_flag & HeaderFlag_First) {
+			CHECK(streams_.find(buffer_page_.header.stream_serial_num) == streams_.end());
+			streams_[buffer_page_.header.stream_serial_num] = StreamInfo();
+			std::cout << "new stream: " << buffer_page_.header.stream_serial_num << std::endl;
+		}
+		CHECK(streams_.find(buffer_page_.header.stream_serial_num) != streams_.end());
+		StreamInfo& stream = streams_[buffer_page_.header.stream_serial_num];
+		
+		// pack packets: join seg table with size 255 and first with <255, each is one packet
+		size_t offset = 0;
+		uint32_t len = 0;
+		for(uint8_t segment_i = 0; segment_i < buffer_page_.header.page_segments_num; ++segment_i) {
+			len += buffer_page_.segment_table[segment_i];
+			if(buffer_page_.segment_table[segment_i] < 255) {
+				// new packet
+				Packet packet;
+				packet.data = buffer_page_.data + offset;
+				packet.data_len = len;
+				if(stream.packet_counts_ == 0)
+					CHECK_ERR(packet.parse_id());
+				else if(stream.packet_counts_ == 1)
+					CHECK_ERR(packet.parse_comment());
+				else if(stream.packet_counts_ == 2)
+					CHECK_ERR(packet.parse_setup());
+				else
+					CHECK_ERR(packet.parse_audio());
+				++stream.packet_counts_;
+				++packet_counts_;
+				offset += len;
+				len = 0;
+			}
+		}
+		assert(len == 0 && offset == buffer_page_.data_len);
+		
+		if(buffer_page_.header.header_type_flag & HeaderFlag_Last) {
+			streams_.erase(buffer_page_.header.stream_serial_num);
+		}
+		
+		return OkOrError();
 	}
 };
 
