5a27cb22d4f5df66f263594fda8ca227542feda0
Albert Zeyer <albzey@gmail.com>
2019-01-08 17:14:57 +0100

    crc

---
+++ src/crctable.h
@@ -0,0 +1,13 @@
+//
+//  crctable.h
+//  ParseOggVorbis
+//
+//  Created by Albert Zeyer on 08.01.19.
+//  Copyright © 2019 Albert Zeyer. All rights reserved.
+//
+
+#ifndef crctable_h
+#define crctable_h
+
+
+#endif /* crctable_h */
