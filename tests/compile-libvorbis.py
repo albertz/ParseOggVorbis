28bd5be4aa28b46efa0c012ee1dc43923eb57ae8
Albert Zeyer <zeyer@cs.rwth-aachen.de>
2019-01-18 23:16:12 +0100

    compile

--- tests/compile-libvorbis.py
+++ tests/compile-libvorbis.py
@@ -18,17 +18,35 @@ def call(args):
 assert os.path.exists(libvorbis_dir)
 assert os.path.exists(libogg_dir)
 
-c_files = [
+ogg_c_files = [
+    "bitwise.c",
+    "framing.c"
+]
+vorbis_c_files = [
+    # lib files
+    "info.c",
     "window.c",
     "floor0.c", "floor1.c",
     "codebook.c",
     "sharedbook.c",
     "block.c",
     "registry.c",
-    "envelope.c"
+    "envelope.c",
+    "bitrate.c",
+    "psy.c",
+    "smallft.c",
+    "mapping0.c",
+    "res0.c",
+    "lpc.c",
+    "lsp.c",
+    "mdct.c",
+    "synthesis.c",
+    # vorbisfile extra
+    "vorbisfile.c"
 ]
 cmd = ["cc"]
 cmd += ["-I", "%s/include" % libogg_dir, "-I", "%s/include" % libvorbis_dir]
-cmd += ["%s/lib/%s" % (libvorbis_dir, fn) for fn in c_files]
+cmd += ["%s/src/%s" % (libogg_dir, fn) for fn in ogg_c_files]
+cmd += ["%s/lib/%s" % (libvorbis_dir, fn) for fn in vorbis_c_files]
 cmd += ["libvorbis-demo.c"]
 check_call(cmd)
