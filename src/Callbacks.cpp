8268b30e78255167281dffd80caeb3691c74045c
Albert Zeyer <zeyer@cs.rwth-aachen.de>
2019-01-21 11:09:27 +0100

    callbacks api

---
+++ src/Callbacks.cpp
@@ -0,0 +1,9 @@
+//
+//  Callbacks.cpp
+//  ParseOggVorbis
+//
+//  Created by i6user on 21.01.19.
+//  Copyright © 2019 Albert Zeyer. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ src/Callbacks.h
@@ -0,0 +1,13 @@
+//
+//  Callbacks.h
+//  ParseOggVorbis
+//
+//  Created by i6user on 21.01.19.
+//  Copyright © 2019 Albert Zeyer. All rights reserved.
+//
+
+#ifndef Callbacks_h
+#define Callbacks_h
+
+
+#endif /* Callbacks_h */
